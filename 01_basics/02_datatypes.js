912b228c1f050963605e04b451fdb5fb50f7ec63
Sheetal <sheetalpannu15@gmail.com>
2022-12-31 05:05:13 +0000

    Conversion of datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all JS code as newer version
+
+// alert( 3 + 3 ) // we are using nodejs, not browser
+
+console.log(3 
+    + 3) // not preferred, code readability should be high
+
+console.log("Sheetal")
+
+
+let name = "Sheetal"
+let age = 19
+let isLoggedIn = false
+let state;
+
+
+// number => 2 to power 53
+// bigint => for large numbers (ex: trading, stock market , reddit)
+// string => "_"
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => for unique
+
+
+// object
+
+console.log(typeof null); //object
+console.log(typeof undefined); // undefined
